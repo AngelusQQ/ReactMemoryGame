88a7427b3d1b8abf3c7c75bd65942ed8d8eb0fb0
William <william.ma9215@gmail.com>
2018-10-27 19:56:22 -0400

    Almost Done

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import chrome from './images/chrome-logo.png';
 import firefox from './images/firefox-logo.png';
 import edge from './images/microsoft-edge-logo.png';
 import safari from './images/safari-icon.png';
+import blank from './images/question.png';
 
 const array = [chrome, chrome, firefox, firefox, edge, edge, safari, safari];
 
@@ -19,26 +20,66 @@ for(let j = 0; j < 8; j++) {
 
 class App extends Component {
   state = {
-    numClicked: 0
+    "global": 0,
+    "ansOne": "",
+    "ansTwo": "",
+    "1": false,
+    "2": false,
+    "3": false,
+    "4": false,
+    "5": false,
+    "6": false,
+    "7": false,
+    "8": false
   }
 
-  rotateCard = () => {
-    console.log("abc");
-    const c = document.getElementById("chrome");
-    c.style.opacity = 0.2;
+  default = () => {
+    this.setState({
+      "global": 0,
+      "20": "",
+      "40": "",
+      "1": false,
+      "2": false,
+      "3": false,
+      "4": false,
+      "5": false,
+      "6": false,
+      "7": false,
+      "8": false
+    })
+  }
+
+  rotateCard = (num) => {
+    console.log(num.target.alt);
+    this.setState({
+      [num.target.id]: true,
+      "global": this.state.global += 1,
+    });
+    if(this.state.global === 1) {
+
+      this.setState({ [this.state.global * 20]: num.target.alt })
+    }
+    console.log(this.state[this.state.global * 20]);
+    if(this.state.global === 2) {
+      setTimeout(this.default, 2000);
+    }
   }
 
   render() {
+    let number = 0;
     return (
       <div className="App">
         {array.map(a => {
+        number += 1;
         let temp = "";
+        const truthy = false;
         a.includes("chrome") ? temp = "chrome"
         : a.includes("firefox") ? temp = "firefox"
         : a.includes("safari") ? temp = "safari"
         : a.includes("microsoft") ? temp = "edge"
         : temp = null;
-        return <Card onClick={this.rotateCard} id={temp} src={a} />
+        console.log(this.state[number], temp);
+        return <Card key={number} onClick={this.rotateCard.bind(number)} alt={temp} id={number.toString()} hidden={this.state[number]} blanksrc={blank} src={a} />
         })}
       </div>
     );
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,11 @@
 import React from "react";
 import "./Card.css";
 
-const Card = props => (
-  <img id={props.id} onClick={props.onClick} className="Card" src={props.src} alt="temp" height="256" width="256" />
-)
-
+const Card = props => {
+  return (!props.hidden ?
+    <img id={props.id} onChange={props.onChange} onClick={props.onClick} className="Card" src={props.blanksrc} alt={props.alt} height="256" width="256" />
+  :
+    <img id={props.id} onChange={props.onChange} onClick={props.onClick} className="Card" src={props.src} alt={props.alt} height="256" width="256" />
+  )
+}
 export default Card;
