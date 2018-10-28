826329f7af8bf173528a52f6f6f8575dc6477a74
William <william.ma9215@gmail.com>
2018-10-27 22:23:11 -0400

    React Router Dom

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ import firefox from './images/firefox-logo.png';
 import edge from './images/microsoft-edge-logo.png';
 import safari from './images/safari-icon.png';
 import blank from './images/question.png';
-// import correct from './sounds/correct.mp3';
-// import wrong from './sounds/wrong.mp3';
-// import winner from './sounds/winner.mp3';
+import correct from './sounds/correct.mp3';
+import wrong from './sounds/wrong.mp3';
+import winner from './sounds/winner.mp3';
 
 const array = [chrome, chrome, firefox, firefox, edge, edge, safari, safari];
 
@@ -51,14 +51,13 @@ class App extends Component {
     "8": false
   }
 
-  // soundCorrect = new Audio(correct);
-  // soundWrong = new Audio(wrong);
-  // soundWinner = new Audio(winner);
+  soundCorrect = new Audio(correct);
+  soundWrong = new Audio(wrong);
+  soundWinner = new Audio(winner);
 
   changeState = (key, value) => {
     return new Promise((resolve, reject) => {
       this.setState({ [key]: value });
-      // console.log(this.state[key]);
       resolve(this);
       if(!this.state[key]) { reject("NO KEY PRESENT"); }
     })
@@ -88,7 +87,7 @@ class App extends Component {
     });
 
     if(this.state.global === 1) {
-      // this.setState({ "cardOne": event.target.alt })
+      this.setState({ "cardOne": event.target.alt })
       this.changeState("cardOne", event.target.alt).then(function(event) {
       });
     }
@@ -101,16 +100,16 @@ class App extends Component {
 
           event.changeState("correct", event.state.correct += 1).then(function(foo) {
             if(foo.state.correct == 4) {
-              // foo.soundWinner.play();
+              foo.soundWinner.play();
               alert("WINNER");
               scramble();
               setTimeout(foo.default, 1500);
             } else {
-              // foo.soundCorrect.play();
+              foo.soundCorrect.play();
             }
           })
         } else {
-          // event.soundWrong.play();
+          event.soundWrong.play();
           setTimeout(event.default, 500);
         }
       });
